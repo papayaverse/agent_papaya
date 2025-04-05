6eeefa5444a93eee4210c4c91a105425b77860a4
Ram Kripa <rammkripa@uchicago.edu>
2025-04-05 12:48:58 -0500

    new data

--- background.js
+++ background.js
@@ -140,8 +140,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const domain = message.domain;
     // Wait for the button data to be loaded before responding
     buttonDataPromise.then(() => {
-      //sendResponse(buttonData[domain]); // change
-      sendResponse(null); // change
+      sendResponse(buttonData[domain]); // change
+      //sendResponse(null); // change
     }).catch(error => {
       console.error('Error sending button data:', error);
       sendResponse({});
@@ -190,8 +190,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       .catch((error) => sendResponse({ success: false, error: error.message }));
     return true; // Respond asynchronously
   } else if (message.action === "getCachedButtonData") {
-    sendResponse(null); // change
-    //sendResponse(buttonCache[message.domain] || null); // change
+    //sendResponse(null); // change
+    sendResponse(buttonCache[message.domain] || null); // change
     return true;
   } else if (message.action === "cacheButtonData") {
     console.log("Caching button data for", message.domain); // change
